65088a924feb1f6973312444ee899fd84844291b
Robert Marek <r.a.marek@icloud.com>
2024-02-05 21:56:11 +0100

    first program "Hello World"

--- CSharpPlayground/Program.cs
+++ CSharpPlayground/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; // inportin standart libray, class, and functions 
+
+class hello // Initiates the definition of a class named "hello"
+{
+    static void Main() //This is the main method, which serves as the entry point for the program. All instructions in the program will be executed from this point onward.
+        
+        //program code
+    {
+     
+        Console.WriteLine("Hello, World!"); // This funkcion displays the string "Hello, World!" in the console.
+
+
+        
+        Console.ReadLine(); //  Waits for the Enter key to be pressed before terminating the program.
+
+    }
+}
